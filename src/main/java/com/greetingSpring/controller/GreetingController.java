4e33f8e498fa3f0a2d6fc62f6c5e599a7b99e3a8
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-11-25 00:46:50 +0530

    Added greeting code to master

---
+++ src/main/java/com/greetingSpring/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.greetingSpring.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.greetingSpring.model.Greeting;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(name="firstName",defaultValue="World") String firstName) {
+		return new Greeting((int) counter.incrementAndGet(),String.format(template, firstName));
+	}
+}
